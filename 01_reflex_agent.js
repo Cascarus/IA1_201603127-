9b560703ba9584065268f95d915bc43b486a0423
Cascarus <cascarus2@gmail.com>
2022-06-08 00:44:42 -0600

    update

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -8,25 +8,35 @@ function reflex_agent(location, state){
 }
 
 function test(states){
-      	var location = states[0];		
-      	var state = states[0] == "A" ? states[1] : states[2];
-      	var action_result = reflex_agent(location, state);
-      	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
+	var locaction, state, action_result;
+
+	while (conta <= 8){
+		locaction = states[0];		
+      	state = states[0] == "A" ? states[1] : states[2];
+      	action_result = reflex_agent(location, state);
+      	
       	if (action_result == "CLEAN"){
         	if (location == "A") states[1] = "CLEAN";
          	else if (location == "B") states[2] = "CLEAN";
       	}
       	else if (action_result == "RIGHT") states[0] = "B";
       	else if (action_result == "LEFT") states[0] = "A";
-	var random = Math.round(Math.random()) + 1;
-	states[random] = "DIRTY";
-	console.log(states);
-	if(states[1] == "CLEAN" && states[2] == "CLEAN"){
-		console.log("FIN");
-	}else{
+		var random = Math.round(Math.random()) + 1;
+		states[random] = "DIRTY";
+		console.log(states);
+		if(states[1] == "CLEAN" && states[2] == "CLEAN"){
+			console.log("FIN");
+		}else{
+			setTimeout(function(){ test(states); }, 2000);
+		}
+		
+		document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
 		setTimeout(function(){ test(states); }, 2000);
+		conta = conta + 1;
 	}
+      	
 }
 
 var states = ["A","DIRTY","DIRTY"];
+var conta = 0;
 test(states);
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # IA1_201603127-
-Inteligencia Artificial, Vacaciones 2022 - 201603127
+Inteligencia Artificial, Vacaciones 2022
+Luis Alfonso Ordoñez Carrillo
+201603127
 
 ## Tareas
 - [Tarea 1 - Reflex Agent](https://cascarus.github.io/IA1_201603127-/01_reflex_agent.html)
